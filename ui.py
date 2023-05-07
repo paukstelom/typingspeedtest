3f1e6736682d4540fb2746917f9e53bd5965b3f1
paukstello <paukstelom@gmail.com>
2023-05-07 16:54:50 +0300

    Added countdown display, worked on main test page design

--- ui.py
+++ ui.py
@@ -1,8 +1,10 @@
-from tkinter import Tk, Label, Button, Canvas
+from math import floor
+from tkinter import Tk, Label, Button, Canvas, Entry
+
+TEST_TIME_IN_SECONDS = 60
 
 
 class Display:
-
     def __init__(self):
         self.window = Tk()
         self.highscores = [
@@ -19,7 +21,7 @@ class Display:
     def window_setup(self):
         self.window.title('Typing Speed Test')
         self.window.config(padx=30, pady=30)
-        self.window.eval('tk::PlaceWindow . center')
+        self.window.geometry('800x500')
         self.window.resizable(False, False)
 
     def clear_window(self):
@@ -29,7 +31,7 @@ class Display:
     def create_home_screen(self):
         def start_test():
             self.clear_window()
-            self.start_typing_test()
+            self.typing_test()
 
         def display_info():
             self.clear_window()
@@ -41,7 +43,7 @@ class Display:
             self.show_highscores()
 
         home_screen = Canvas()
-        home_screen.grid(pady=30, padx=30)
+        home_screen.pack(anchor='center', pady=80)
 
         label = Label(home_screen, text='Welcome to typing speed tester!')
         label.grid(column=1, row=0, pady=30, padx=200)
@@ -55,19 +57,14 @@ class Display:
         hs_btn = Button(home_screen, text='High scores', width=20, command=show_highscores)
         hs_btn.grid(column=1, row=2, pady=10, padx=200)
 
-    def countdown(self, seconds: int, text_holder: Label) -> None:
-        text_holder.configure(text=str(seconds))
-        if seconds > 0:
-            self.window.after(1000, self.countdown, seconds - 1, text_holder)
-
     def show_highscores(self):
         canvas = Canvas()
+        canvas.pack(anchor='center', pady=80)
 
         def testfunc():
             canvas.destroy()
             self.create_home_screen()
 
-        canvas.grid(pady=30, padx=30)
         label = Label(canvas, text='Highscores:')
         label.grid(column=1, row=0, pady=10, padx=200)
 
@@ -78,27 +75,51 @@ class Display:
         return_btn = Button(canvas, text='Back', command=testfunc)
         return_btn.grid(column=1, row=6, pady=10, padx=200)
 
-    def start_typing_test(self):
+    def typing_test(self):
         canvas = Canvas()
+        canvas.pack(anchor='center', pady=30, padx=30)
+
+        label = Label(canvas, text='Test goes here')
+        label.grid(column=1, row=1, pady=100, padx=20)
+
+        text_input = Entry(canvas, width=50)
+        text_input.grid(column=0, columnspan=3, row=2)
+
+        misc_label = Label(canvas, text='Something here')
+        misc_label.grid(column=2, row=0)
 
         def testfunc():
             canvas.destroy()
             self.create_home_screen()
 
-        canvas.grid(pady=30, padx=30)
-        label = Label(canvas, text='Test goes here')
-        label.grid(column=1, row=0, pady=30, padx=200)
         return_btn = Button(canvas, text='Back', command=testfunc)
-        return_btn.grid(column=1, row=3, pady=10, padx=200)
+        return_btn.grid(column=1, row=3, pady=50, padx=200)
+
+        timer = Label(canvas, text=f'')
+        timer.grid(column=0, row=0)
+
+        def test_countdown(seconds):
+            min_count = floor(seconds / 60)
+            sec_count = seconds % 60
+            if sec_count < 10:
+                sec_count = f'0{sec_count}'
+
+            display_time = f'TIME LEFT: {min_count}:{sec_count}'
+            timer.configure(text=display_time)
+
+            if seconds > 0:
+                self.window.after(1000, test_countdown, seconds - 1)
+
+        test_countdown(TEST_TIME_IN_SECONDS)
 
     def show_info(self):
         canvas = Canvas()
+        canvas.pack(anchor='center', pady=80)
 
         def testfunc():
             canvas.destroy()
             self.create_home_screen()
 
-        canvas.grid(pady=30, padx=30)
         label = Label(canvas, text='Info goes here')
         label.grid(column=1, row=0, pady=30, padx=200)
         return_btn = Button(canvas, text='Back', command=testfunc)
